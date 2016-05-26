a612c8cd04f285e189d02ea988e3bc8f194dbe17
Yeshe Tenley <manyoso@yahoo.com>
2016-05-26 09:04:16 -0400

    Reorganize and document.

--- README.md
+++ README.md
@@ -7,40 +7,39 @@ Practicing the Excellent Path,
 Praised by the Wise.
 ```
 
-### Tibetan support for Helmut Schmid's Tree Tagger
+### Tibetan Part of Speech Tool based upon Helmut Schmid's Tree Tagger
 
-This repository contains the paramater file and training corpus for Tibetan language support for the
-Tree Tagger tool from http://www.cis.uni-muenchen.de/~schmid/tools/TreeTagger/
+This repository contains the parameter file and training corpus for Tibetan
+language support for the Tree Tagger tool from http://www.cis.uni-muenchen.de/~schmid/tools/TreeTagger/
 
-The lexicon, corpus, and enumeration of tags are in the tibetan/ directory and come from
-http://larkpie.net/tibetancorpus/ courtesy of Dr. Nathan Hill.  These documents needed to be edited
-to remove blank lines and add a end-of-sentence tag to the lexicon of '།'
+The lexicon, corpus, and enumeration of tags are in the training/ directory and
+come from http://larkpie.net/tibetancorpus/ courtesy of Dr. Nathan Hill.  These
+documents needed to be edited to remove blank lines and add a end-of-sentence
+tag to the lexicon of '།'
 
-### Syllable Tagger
-The resultant tibetan-syllable-utf8.par parameter file was generated with this command:
+### Part of Speech Tool
 
-```
-bin/train-tree-tagger -st sent -utf8 tibetan/2016-04-30-syllables-lexicon.txt tibetan/tibetan-syllables-tags.txt tibetan/2016-04-30-syllables-training.txt lib/tibetan-syllables-utf8.par
-```
-
-To test:
+The python script tibetan-pos.py acts as a wrapper/driver for the Tree Tagger
+and is invoked like so:
 
 ```
-cat tibetan/test/syllables.txt | cmd/tree-tagger-tibetan-syllables
+./tibetan-pos.py --file test/test.txt
 ```
 
-### POS Tagger
-The resultant tibetan-utf8.par parameter file was generated with this command:
+The optional flags dictate the form of the output.  With no flags all parts of
+speech operations are run and the output displayed to stdout.
 
-```
-bin/train-tree-tagger -st sent -utf8 tibetan/2016-04-30-pos-lexicon.txt tibetan/tibetan-pos-tags.txt tibetan/2016-04-30-pos-training.txt lib/tibetan-utf8.par
-```
+Required arguments are:
 
-To test:
+  script          Specify the tibetan script directly on the command line
+OR
+  -f --file       Specify the file containing the tibetan script
 
-```
-cat tibetan/test/pos.txt | cmd/tree-tagger-tibetan
-```
+Optional flags are:
+
+  -s --syllables  Output tagged syllables only
+  -w --words      Output word break of input only
+  -p --pos        Output tagged part of speech only
 
 ### Dedication
 
--- tibetan-pos.py
+++ tibetan-pos.py
@@ -10,36 +10,45 @@ import sys
 import subprocess
 import shutil
 
-# The word breaker involves the following steps:
+# The tibetan pos tool involves the following steps:
 #
 # 1. Text as you find it in the wild.
 #    བྲམ་ཟེ་དེས་རྒྱལ་པོ་ལ་འདི་སྐད་ཅེས་སྨྲས་སོ༎
 #    རྒྱལ་པོ་ཆེན་པོ་ཁྱོད་ཀྱི་ལུས་ལ་མར་མེ་སྟོང་བཙུགས་ཏེ།
 #    མཆོད་པ་བྱེད་ནུས་ན་ཆོས་བསྟན་པར་བྱའོ་ཞེས་སྨྲས་པ་དང་ །
-# 2. The word breaker reformats into something suiteable to the Tree Tagger
+# 2. The tool reformats into something suiteable to the Tree Tagger
 #    (achieved by replacing ༎ with །། and then adding a space after each ་ and
 #    before and after །).
 #    བྲམ་ ཟེ་ དེས་ རྒྱལ་ པོ་ ལ་ འདི་ སྐད་ ཅེས་ སྨྲས་ སོ ། །
 #    རྒྱལ་ པོ་ ཆེན་ པོ་ ཁྱོད་ ཀྱི་ ལུས་ ལ་ མར་ མེ་ སྟོང་ བཙུགས་ ཏེ །
 #    མཆོད་ པ་ བྱེད་ ནུས་ ན་ ཆོས་ བསྟན་ པར་ བྱའོ་ ཞེས་ སྨྲས་ པ་ དང་ །
 # 3. Feed this as input to the Tree Tagger which produces output in the form
-#    of vertical columns of the script and syllables and then apply the following
-#    rule: If a syllable ends with འི་, འོ་, ར་ or ས་ AND the syllable is tagged by the
-#    word breaker with ES or SS, then insert a space before the respective འི་, འོ་,
-#    ར་ or ས་
-# 4. Which transforms the output of the word breaker to this:
+#    of vertical columns of the script and syllables
+# 4. If we are in syllables mode we output the syllables now and stop and if not
+#    we...
+# 5. Apply the following rule: If a syllable ends with འི་, འོ་, ར་ or ས་ AND the
+#    syllable is tagged by the word breaker with ES or SS, then insert a space
+#    before the respective འི་, འོ་, ར་ or ས་ (note: we also apply the rule to the
+#    form without the '་' character
+# 5. Which transforms the output to this:
 #    བྲམ་ཟེ་ དེ ས་ རྒྱལ་པོ་ ལ་ འདི་ སྐད་ ཅེས་ སྨྲས་ སོ ། །
 #    རྒྱལ་པོ་ ཆེན་པོ་ ཁྱོད་ ཀྱི་ ལུས་ ལ་ མར་མེ་ སྟོང་ བཙུགས་ ཏེ །
 #    མཆོད་པ་ བྱེད་ ནུས་ ན་ ཆོས་ བསྟན་པ ར་ བྱ འོ་ ཞེས་ སྨྲས་པ་ དང་ །
+# 6. If we are in words mode we output the words now and stop and if not we...
+# 7. Feed this as input to the Tree Tagger which produces output in the form of
+#    vertical columns of the script and POS tags
 
 def main():
   # Argument parsing
   script = None
 
-  parser = argparse.ArgumentParser(prog='wordbreaker', description='Word breaker for Tibetan script powered by Tree Tagger.')
+  parser = argparse.ArgumentParser(prog="tibetan-pos-tool", description="Part of speech tool for Tibetan script powered by Tree Tagger.")
   group = parser.add_mutually_exclusive_group(required=sys.stdin.isatty())
-  group.add_argument('script', nargs="?", default="")
-  group.add_argument('-file')
+  group.add_argument("script", nargs="?", default="")
+  group.add_argument("-f", "--file")
+  parser.add_argument("-s", "--syllables", help="Output tagged syllables only", action="store_true")
+  parser.add_argument("-w", "--words", help="Output word break of input only", action="store_true")
+  parser.add_argument("-p", "--pos", help="Output tagged part of speech only", action="store_true")
   args = parser.parse_args()
 
   script = None
@@ -54,8 +63,14 @@ def main():
     os._exit(1)
 
   if args.file:
-    with codecs.open(args.file, "r", encoding="utf-8") as scriptfile:
+    with open(args.file, "r") as scriptfile:
       script = scriptfile.read()
+
+  all = not args.syllables and not args.words and not args.pos
+
+  if all:
+    print "Original script..."
+    print re.sub(r"\n$", "", script) + "\n"
 
   # Replace '༎' with two '།' chars
   newscript = re.sub(r"༎", "།།", script)
@@ -66,8 +81,22 @@ def main():
   # Replace '།' followed by non-whitespace with '།' followed by whitespace
   newscript = re.sub(r"།(?=\S)", "། ", newscript)
 
+  if all:
+    print "Pre-formatted script..."
+    print re.sub(r"\n$", "", newscript) + "\n"
+
   # Run the tree tagger on the newscript
-  wordscript = callExternalCommand(treeTaggerCommand(newscript))
+  wordscript = callExternalCommand(treeTaggerSyllableCommand(newscript))
+  # Remove last newline
+  wordscript = re.sub(r"\n$", "", wordscript)
+
+  if all:
+    print "Syllable tags..."
+    print re.sub(r"\n$", "", wordscript) + "\n"
+  elif args.syllables:
+    print wordscript
+    os._exit(0)
+
   # Break it into a list of strings
   wordscript = wordscript.split("\n")
   # Remove empty lines
@@ -82,7 +111,7 @@ def main():
     # word breaker with ES or SS, then insert a space before the respective འི་, འོ་,
     # ར་ or ས་
     if syllable == "ES" or syllable == "SS":
-      script = re.sub(r"(འི་|འོ་|ར་|ས་)$", r" \1 ", script)
+      script = re.sub(r"(འི་|འོ་|ར་|ས་|འི|འོ|ར|ས)$", r" \1 ", script)
     elif syllable == "S" or syllable =="E" or syllable == "sent":
       script += " "
     newwordscript += script
@@ -92,11 +121,31 @@ def main():
   # Remove last newline
   newwordscript = re.sub(r"\n$", "", newwordscript)
 
-  print newwordscript
+  if all:
+    print "Word and sentence breaker..."
+    print re.sub(r"\n$", "", newwordscript) + "\n"
+  elif args.words:
+      print newwordscript
+      os._exit(0)
 
-def treeTaggerCommand(script):
+  # Run the tree tagger on the newwordscript
+  newwordscript = callExternalCommand(treeTaggerPOSCommand(newwordscript))
+  # Remove last newline
+  newwordscript = re.sub(r"\n$", "", newwordscript)
+
+  if all:
+    print "Part of speech tags..."
+    print re.sub(r"\n$", "", newwordscript)
+  elif args.pos:
+    print newwordscript
+
+def treeTaggerSyllableCommand(script):
   dir = os.path.dirname(os.path.realpath(__file__))
-  return "echo \"" + script + "\" | " + dir + "/../cmd/tree-tagger-tibetan-syllables"
+  return "echo \"" + script + "\" | " + dir + "/scripts/tree-tagger-tibetan-syllables"
+
+def treeTaggerPOSCommand(script):
+  dir = os.path.dirname(os.path.realpath(__file__))
+  return "echo \"" + script + "\" | " + dir + "/scripts/tree-tagger-tibetan"
 
 def callExternalCommand(command):
   process = subprocess.Popen(command, shell=True, stdout=subprocess.PIPE, stderr=subprocess.PIPE)
